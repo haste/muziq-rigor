2de36b8fef317c43846082bcf20541b0b3692fe0
Trond A Ekseth <troeks@gmail.com>
2010-10-09 13:30:22 +0200

    Add usage output and some minor validation of input.

--- muri.lua
+++ muri.lua
@@ -1,1 +1,27 @@
 package.path = 'libs/?/init.lua;libs/?.lua;' .. package.path
+local metadata = require'metadata'
+local lfs = require'lfs'
+
+local _USAGE = [[Usage: muri DIRECTORY...
+Recursively iterates the DIRECTORY(ies) and validate the media files based on
+defined rules.
+
+Currently supports:
+ - Formats: FLAC, Ogg
+ - Metadata: VorbisComment]]
+
+local print = function(fmt, ...)
+	print(string.format('muri: ' .. fmt, ...))
+end
+
+if(not ...) then
+	print(_USAGE)
+else
+	for i=1, select('#', ...) do
+		local dir = select(i, ...)
+		local mode = lfs.attributes(dir, 'mode')
+		if(mode ~= 'directory') then
+			return print("`%s' is not a directory.", dir)
+		end
+	end
+end
